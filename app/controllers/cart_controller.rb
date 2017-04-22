12baa8cc90ff96938ab6c2eb18d3629c03072a8f
Boris Xiao <borishaw@gmail.com>
2017-04-22 23:03:12 +0000

    added search result page

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,2 @@
+class CartController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
   resources :tools
 
 end
