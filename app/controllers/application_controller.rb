9a19c0f3070acad93cd841fdd73cc85a47605c36
Bruno Farias <bruno.cabral88@hotmail.com>
2017-05-03 11:39:51 -0400

    Fixes bug in session[:cart_items]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,10 @@ class ApplicationController < ActionController::Base
   end
 
   def cart
-    session[:cart_items] ||= {}
+    if session[:cart_items].empty?
+      session[:cart_items] = {}
+    end
+    session[:cart_items]
   end
 
   private
